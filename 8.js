073b5828a0486138f217ffcf2e1a73dc526baff1
NooR Nabiji <noornabiji4@gmail.com>
2018-08-15 00:03:51 +0530

    ES6: Write Higher Order Arrow Functions

---
+++ 8.js
@@ -0,0 +1,13 @@
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers=[16,1764,36]
+  arr=>arr.map().filter().reduce()
+  
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
