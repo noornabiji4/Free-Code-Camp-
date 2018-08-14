6b23f3782b0c09ae8f60c01abbc825e0786e6985
NooR Nabiji <noornabiji4@gmail.com>
2018-08-14 16:23:16 +0530

    ES6: Explore Differences Between the var and let Keywords

---
+++ 1.js
@@ -0,0 +1,11 @@
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+console.log(catName);
+console.log(quote);
+}
+catTalk();
