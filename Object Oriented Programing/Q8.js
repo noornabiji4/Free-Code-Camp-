d67d5e1a5e833da086d08da42a0f01142f0ad3d9
NooR Nabiji <noornabiji4@gmail.com>
2018-08-17 01:24:58 +0530

    Object Oriented Programming: Verify an Object's Constructor with instanceof

---
+++ Object Oriented Programing/Q8.js
@@ -0,0 +1,12 @@
+/* jshint expr: true */
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Add your code below this line
+
+let myHouse = new  House(4);
+myHouse instanceof House;
+
+
