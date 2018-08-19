f52cd91361c4e87771f241faa3ee81ceaaf4e155
NooR Nabiji <noornabiji4@gmail.com>
2018-08-20 01:17:49 +0530

    Reset an Inherited Constructor Property

---
+++ Object Oriented Programing/Q20.js
@@ -0,0 +1,14 @@
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Add your code below this line
+
+
+let duck = new Bird();
+let beagle = new Dog();
+duck.constructor = Bird;
+beagle.constructor = Dog;
