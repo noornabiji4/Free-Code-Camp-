253abffa8727924627fd209878755953d070826f
Nabiji Noor <noornabiji4@gmail.com>
2018-08-28 23:51:26 +0530

    longets string in the sentence

---
+++ Basic Algorithm Scripting/longestStrin.js
@@ -0,0 +1,12 @@
+function findLongestWordLength(str) {
+   var strSplit = str.split(' ');
+   var longestWord = 0;
+   for(var i = 0; i < strSplit.length; i++){
+     if(strSplit[i].length > longestWord){
+       longestWord = strSplit[i].length;
+        }
+  }
+  return longestWord;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
