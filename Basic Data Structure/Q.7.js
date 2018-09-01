a19f6e67a3c1ae09902c26ad8991ce05796f3cb9
Nabiji Noor <noornabiji4@gmail.com>
2018-09-01 13:08:47 +0530

    Copy Array Items Using slice()

---
+++ Basic Data Structure/Q.7.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+    // change code below this line
+    var arr2=arr.slice(2,4);
+    return arr2;
+  }
+  
+  // do not change code below this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
