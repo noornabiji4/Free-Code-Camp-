d4f33b3c049259f8182509c19391f30e4e5663cf
Nabiji Noor <noornabiji4@gmail.com>
2018-09-15 23:50:43 +0530

    Sum All Numbers in a Range

---
+++ Intermediate Algorithm Scripting/Q1.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+    var max = Math.max(arr[0], arr[1]);
+    var min = Math.min(arr[0], arr[1]);
+    var calculate = 0;
+    for (var i=min; i <= max; i++){
+        calculate += i;
+    }
+  return(calculate);
+}
+
+sumAll([1, 4]);
