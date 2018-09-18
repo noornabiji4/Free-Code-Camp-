a8a1045067f2c1419ac66e6caf57822e25438277
Nabiji Noor <noornabiji4@gmail.com>
2018-09-18 17:23:16 +0530

    Use a Mixin to Add Common Behavior Between Unrelated Objects

---
+++ Object Oriented Programing/Q22.js
@@ -0,0 +1,22 @@
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  // Add your code below this line
+  var glideMixin = function (mixin){
+      mixin.glide = function(){
+          
+      }
+  }
+  
+  glideMixin(bird);
+  glideMixin(boat);
+  
+  
+  
