dc12817226e6d26d7fc87ff1876b4ef3b7741164
Nabiji Noor <noornabiji4@gmail.com>
2018-09-19 20:01:34 +0530

    Avoid Mutations and Side Effects Using Functional Programming

---
+++ Functional Programming/Q4.js
@@ -0,0 +1,15 @@
+// the global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Add your code below this line
+
+  return fixedValue+1;
+fixedValue++;
+ 
+  
+  // Add your code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
