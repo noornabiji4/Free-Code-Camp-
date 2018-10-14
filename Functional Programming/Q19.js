da6cc808c12afa31229f26e2cf2594000948b812
Noor <noornabiji4@gmail.com>
2018-10-14 21:52:26 +0530

    Apply Functional Programming to Convert Strings to URL Slugs

---
+++ Functional Programming/Q19.js
@@ -0,0 +1,13 @@
+// the global variable
+var globalTitle = "Winter Is Coming";
+
+// Add your code below this line
+function urlSlug(title) {
+  return title.split(/\W/).filter((arr)=>{
+    return arr
+  }).join('-').toLowerCase()
+  
+}
+// Add your code above this line
+
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
