5a0dee388256e4a7552067ff0f8dd7be6f4ecad9
Noor <noornabiji4@gmail.com>
2018-10-14 22:42:52 +0530

    Introduction to Currying and Partial Application

---
+++ Functional Programming/Q22.js
@@ -0,0 +1,12 @@
+function add(x) {
+    // Add your code below this line
+    return (y)=>{
+      return(z)=>{
+        return x+y+z;
+      }
+  
+    }
+    
+    // Add your code above this line
+  }
+  add(10)(20)(30);
