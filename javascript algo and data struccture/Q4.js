31490761e460c651e1fd087cecfe791aa2e68db9
Noor <noornabiji4@gmail.com>
2018-11-22 23:08:02 +0530

    Telephone Number Validator

---
+++ javascript algo and data struccture/Q4.js
@@ -0,0 +1,7 @@
+function telephoneCheck(str) {
+    // Good luck!
+   var reg = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+     return reg.test(str);;
+  }
+  
+  telephoneCheck("555-555-5555");
