8a02542d07d4cd22975fa6d7934d694ee5b1ce3f
Noor <noornabiji4@gmail.com>
2018-12-18 20:11:05 +0530

    Appending Variables to Strings

---
+++ Basic Javascript/Q28.js
@@ -0,0 +1,11 @@
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "Freecodecamp";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
