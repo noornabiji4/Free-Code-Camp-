022a37e28ea5766e2b0043be7873041b0403ea57
Noor <noornabiji4@gmail.com>
2018-12-25 00:02:55 +0530

    Local Scope and Functions

---
+++ Basic Javascript/Q49.js
@@ -0,0 +1,12 @@
+function myLocalScope() {
+    var myVar; // you shouldn't need to edit this line
+
+    console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+
+// Now remove the console log line to pass the test
