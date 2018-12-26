5de7cfd112059703962fe02a2670c16d54361b34
Noor <noornabiji4@gmail.com>
2018-12-26 23:53:18 +0530

    Assignment with a Returned Value

---
+++ Basic Javascript/Q53.js
@@ -0,0 +1,19 @@
+// Example
+var changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 2;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+var processed = processArg(7);
