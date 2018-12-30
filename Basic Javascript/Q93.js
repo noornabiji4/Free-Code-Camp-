76010b11e86c23f29fed3e82a9e8045faf356c74
Noor <noornabiji4@gmail.com>
2018-12-30 23:56:34 +0530

    Iterate with JavaScript For Loops

---
+++ Basic Javascript/Q93.js
@@ -0,0 +1,10 @@
+// Setup
+var myArray = [];
+var i = 0;
+while (i < 5) {
+    myArray.push(i);
+    i++;
+}
+
+// Only change code below this line.
+
---
+++ Basic Javascript/Q94.js
@@ -0,0 +1,14 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+    ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+// Only change code below this line.
+
